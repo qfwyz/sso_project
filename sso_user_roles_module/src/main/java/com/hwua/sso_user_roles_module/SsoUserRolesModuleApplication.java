1ac2df37e77f02b5109c33bc4d29c7899f402b8a
qfwyz_local <2645313145_local@qq.com>
2019-12-23 15:17:52 +0800

    这是用户角色权限管理系统

---
+++ sso_user_roles_module/src/main/java/com/hwua/sso_user_roles_module/SsoUserRolesModuleApplication.java
@@ -0,0 +1,13 @@
+package com.hwua.sso_user_roles_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoUserRolesModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoUserRolesModuleApplication.class, args);
+    }
+
+}
---
+++ sso_user_roles_module/src/test/java/com/hwua/sso_user_roles_module/SsoUserRolesModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwua.sso_user_roles_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoUserRolesModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
