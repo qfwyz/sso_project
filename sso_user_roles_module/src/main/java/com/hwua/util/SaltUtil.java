4a3ae47218f4fc3e96fec182165c483226db1d12
qfwyz_local <2645313145_local@qq.com>
2019-12-24 15:08:09 +0800

    20191224   11:42

--- sso_user_roles_module/src/main/java/com/hwua/util/SaltUtil.java
+++ sso_user_roles_module/src/main/java/com/hwua/util/SaltUtil.java
@@ -1,13 +1,18 @@
 package com.hwua.util;
 
+import java.security.SecureRandom;
+
 /**
  * 负责生成盐
  */
 public class SaltUtil {
     //要求生成一个十六进制的字符串
     public static String getSalt(){
-       // String[] s = {"0","1","2",.....};
-       // Integer.toHexString()
-        return "";
+     //构建加密类对象
+        SecureRandom secureRandom = new SecureRandom();
+        //构建byte类型的数组
+        byte[] bytes = new byte[20];
+        secureRandom.nextBytes(bytes);
+        return new String(bytes);
     }
 }
